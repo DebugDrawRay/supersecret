a659268c0698e02123b4039db41afba3a7caf466
Raymond John Grice <raymondjgrice@gmail.com>
2016-02-25 12:24:43 -0800

    Added grid integration

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -5,12 +5,13 @@ public class Grid : MonoBehaviour
 {
     [Header("Grid Properties")]
     public float unitSize;
-    public float xUnits;
-    public float yUnits;
+    public int xUnits;
+    public int yUnits;
 
     public GameObject point;
+    public GameObject player;
 
-    public Unit[] gridUnits
+    public Unit[,] gridUnits
     {
         get;
         private set;
@@ -28,37 +29,39 @@ public class Grid : MonoBehaviour
     void Start()
     {
         BuildGrid();
+        SetupPlayer();
     }
 
     void BuildGrid()
     {
         float totalUnits = xUnits * yUnits;
-        gridUnits = new Unit[Mathf.RoundToInt(totalUnits)];
+        gridUnits = new Unit[xUnits, yUnits];
 
         float xStart = transform.localPosition.x - (((xUnits / 2) - 0.5f ) * unitSize);
         float yStart = transform.localPosition.y - (((yUnits / 2) - 0.5f) * unitSize);
         float zStart = transform.localPosition.z;
         for (int i = 0; i < totalUnits; i++)
         {
-            float x = i % xUnits;
-            float y = (i - x) / xUnits;
+            int x = i % xUnits;
+            int y = (i - x) / xUnits;
 
             float xPos = (xStart + (unitSize * x));
             float yPos = (yStart + (unitSize * y));
 
             Vector3 unitPos = new Vector3(xPos, yPos, zStart);
-            gridUnits[i] = new Unit(unitPos, transform);
+            gridUnits[x,y] = new Unit(unitPos, transform);
 
             GameObject newPoint = Instantiate(point) as GameObject;
             newPoint.transform.SetParent(transform);
 
-            newPoint.transform.position = gridUnits[i].position;
+            newPoint.transform.position = gridUnits[x,y].position;
             newPoint.transform.rotation = transform.rotation;
         }
     }
 
-    void Update()
+    void SetupPlayer()
     {
-        Debug.Log(gridUnits[0].position);
+        GridMovement newPlayer = Instantiate(player).GetComponent<GridMovement>();
+        newPlayer.Init(this, new Vector2(3,3));
     }
 }
---
+++ Assets/GridMovement.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using InControl;
+
+public class GridMovement : MonoBehaviour
+{
+    private Grid targetGrid;
+
+    private bool initialized;
+
+    private PlayerActions input;
+    private bool directionHeld;
+
+    public Vector2 gridPosition;
+
+    public void Init(Grid grid, Vector2 position)
+    {
+        targetGrid = grid;
+        gridPosition = position;
+
+        transform.position = targetGrid.gridUnits[Mathf.RoundToInt(position.x), Mathf.RoundToInt(position.y)].position;
+
+        input = new PlayerActions();
+
+        input.Up.AddDefaultBinding(InputControlType.LeftStickUp);
+        input.Down.AddDefaultBinding(InputControlType.LeftStickDown);
+        input.Left.AddDefaultBinding(InputControlType.LeftStickLeft);
+        input.Right.AddDefaultBinding(InputControlType.LeftStickRight);
+
+        initialized = true;
+    }
+
+    void Update()
+    {
+        if(initialized)
+        {
+            MovementListener();
+        }
+    }
+
+    void MovementListener()
+    {
+        float x = input.Move.X;
+        float y = input.Move.Y;
+
+        if (!directionHeld)
+        {
+            if (x >= .2f)
+            {
+                gridPosition.x += 1;
+                directionHeld = true;
+            }
+            if (x <= -.2f)
+            {
+                gridPosition.x -= 1;
+                directionHeld = true;
+            }
+            if (y >= .2f)
+            {
+                gridPosition.y += 1;
+                directionHeld = true;
+            }
+            if (y <= -.2f)
+            {
+                gridPosition.y -= 1;
+                directionHeld = true;
+            }
+        }
+        else
+        {
+            if(x < .5f && x > -.5f && y < .5f && y > -.5f)
+            {
+                directionHeld = false;
+            }
+        }
+
+        if(gridPosition.x < 0)
+        {
+            gridPosition.x = 0;
+        }
+        if(gridPosition.x >= targetGrid.xUnits)
+        {
+            gridPosition.x = targetGrid.xUnits - 1;
+        }
+        if (gridPosition.y < 0)
+        {
+            gridPosition.y = 0;
+        }
+        if (gridPosition.y >= targetGrid.yUnits)
+        {
+            gridPosition.y = targetGrid.yUnits - 1;
+        }
+
+        transform.position = transform.position = targetGrid.gridUnits[Mathf.RoundToInt(gridPosition.x), Mathf.RoundToInt(gridPosition.y)].position;
+    }
+
+}
