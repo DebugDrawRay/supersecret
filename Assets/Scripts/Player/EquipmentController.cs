74382b2152246e6b6c41a6352ea9d11ec7879f73
Raymond John Grice <raymondjgrice@gmail.com>
2016-03-22 12:56:16 -0700

    Reorganized scripts, added player attachment controller

---
+++ Assets/Scripts/Player/EquipmentController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using Spine;
+
+public class EquipmentController : MonoBehaviour
+{
+    public GameObject headAttachment;
+    public PlayerAttachment equippedHead;
+
+    public GameObject bodyAttachment;
+    public PlayerAttachment equippedBody;
+
+    public GameObject bikeAttachment;
+    public PlayerAttachment equippedBike;
+
+    public GameObject leftAttachment;
+    public PlayerAttachment equippedLeft;
+
+    public GameObject rightAttachment;
+    public PlayerAttachment equippedRight;
+
+    public void Init()
+    {
+        EquipAttachments();
+    }
+
+    public void EquipAttachments()
+    {
+        if (headAttachment)
+        {
+        }
+        if (bodyAttachment)
+        {
+        }
+        if (bikeAttachment)
+        {
+        }
+        if (leftAttachment)
+        {
+        }
+        if (rightAttachment)
+        {
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerAttachmentController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerAttachmentController : MonoBehaviour
+{
+    public enum state
+    {
+        Idle,
+        Collision,
+        Destroyed
+    }
+
+    public state currentState;
+
+    private bool initialized;
+
+    void Init()
+    {
+        initialized = true;
+    }
+    void Update()
+    {
+        if(initialized)
+        {
+            RunStates();
+        }
+    }
+    void RunStates()
+    {
+        switch(currentState)
+        {
+            case state.Idle:
+                break;
+            case state.Collision:
+                break;
+            case state.Destroyed:
+                break;
+        }
+    }
+}
