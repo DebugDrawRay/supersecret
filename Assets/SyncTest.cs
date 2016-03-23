70289b6db34cdcd596f477a76341d467d01a5d56
Soren Laulainen <soren@spite.house>
2016-03-23 14:33:13 -0700

    Test: Camera shake on beat

---
+++ Assets/SyncTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SyncTest : MonoBehaviour {
+
+	Camera mainCamera;
+	QuadCam cameraScript;
+
+	// Use this for initialization
+	void Start () {
+		mainCamera = FindObjectOfType<Camera>();
+		cameraScript = mainCamera.GetComponent<QuadCam>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void beatSync(AkEventCallbackMsg in_callbackInfo) {
+		AkCallbackManager.AkMusicSyncCallbackInfo AkEventCallbackInfo = (AkCallbackManager.AkMusicSyncCallbackInfo)in_callbackInfo.info;
+		Debug.Log("Beat.");
+		cameraScript.CameraShake();
+
+	}
+
+		
+}
